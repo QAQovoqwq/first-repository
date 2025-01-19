5227bf6fc2ccb4432cb8f951afccdd83cfe8e62c
QAQovoqwq <j.zhang03202417@arts.ac.uk>
2025-01-19 21:55:24 +0000

    Update sketch.js
    +range of influence

--- sketch.js
+++ sketch.js
@@ -18,6 +18,12 @@
 
 let circles = [];
 
+let graymaxradius = 100;
+let expansionSpeed = 2;
+let currentradius = 0;
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   generatecircles();
@@ -27,8 +33,9 @@ function draw() {
   background(240);
   movecircle();
   drawcircle();
+  rangeinfluence();
+  updateRadius();
   
-
   
 }
 
@@ -36,21 +43,21 @@ function generatecircles(){
   let Propagationprobability;   //传播概率
   let Decayprobability;         //衰退概率
   let propagationrange;         //传播范围
-  let Diffusionrate;            //扩散快慢
-  let Networkdensity;           //网络密度
+  let Diffusionrate = 1;            //扩散快慢
+  let Networkdensity = 1;           //网络密度
   let color;
 
   let no = Networkdensity;
 
 
 
-  for(let i = 0; i < 20; i++) {
+  for(let i = 0; i < 20*Networkdensity; i++) {
     let circledata = {
       no:i+1,
       positonX:random(50,windowWidth-50),
       positonY:random(50,windowHeight-50),
-      dx:random(-5, 5), //x方向速度
-      dy:random(-5, 5), //y方向速度
+      dx:random(-5*Diffusionrate, 5*Diffusionrate), //x方向速度
+      dy:random(-5*Diffusionrate, 5*Diffusionrate), //y方向速度
       radius:50,
       Propagationprobability,
       Decayprobability,
@@ -97,6 +104,38 @@ function drawcircle()
     text(circle.no, circle.positonX, circle.positonY);
 
   }
+}
+
+function rangeinfluence()
+{
 
 
+  for(let circle of circles)
+  {
+    noFill(); 
+    strokeWeight(2);
+
+    stroke(200); 
+    ellipse(circle.positonX, circle.positonY, currentradius * 2);
+
+    if (currentradius > graymaxradius * 0.6) {
+      stroke(150); 
+      ellipse(circle.positonX, circle.positonY, currentradius * 0.8 * 2); // 第二层圆
+    }
+
+    if (currentradius > graymaxradius * 0.3) {
+      stroke(100); 
+      ellipse(circle.positonX, circle.positonY, currentradius * 0.5 * 2); // 第三层圆
+    
+    }
+  
+  }
+}
+
+function updateRadius() {
+  currentradius += expansionSpeed; 
+  if (currentradius > graymaxradius) 
+  {
+    currentradius = 0; 
+  }
 }
